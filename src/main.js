975704250df37d42cac100cb2ba46e5abcc015d2
Luz Ma <luzhersor@gmail.com>
2022-09-07 16:46:02 -0500

    Finished

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-const API = "https://youtube-v31.p.rapidapi.com/search?channelId=https%3A%2F%2Fwww.youtube.com%2Fuser%2FGameofThrones&part=snippet%2Cid&order=date&maxResults=5"
+
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UC5xfbRQ_7d70RVIv_LdjyyA&part=snippet%2Cid&order=date&maxResults=5";
+
+const content = null || document.getElementById("content");
 
 const options = {
     method: 'GET',
@@ -7,7 +10,6 @@ const options = {
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
-
 
 //Esto era una promesa que borramos
 /*fetch('', options)
@@ -36,20 +38,27 @@ async function fetchData(urlApi) {
         const videos = await fetchData(API);
         //creamos un template del HTML
         let view = `
-        ${videos.items.map(video => `
-        <div class="group relative">
-                    <div
-                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                        <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
-                    </div>
-                    <div class="mt-4 flex justify-between">
-                        <h3 class="text-sm text-gray-700">
-                            <span aria-hidden="true" class="absolute inset-0"></span>
-                            House of Dragons
-                        </h3>
-                    </div>
-                </div>`)
-            } catch {
+        ${videos.items.map(video =>
+            `<div class="group relative">
+        <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                House of Dragons
+                ${video.snippet.title}
+            </h3>
+        </div>
+    </div>
+    `).slice(0, 4).join('')}
+    `;
 
+        content.innerHTML = view;
+
+    }
+    catch (error) {
+        console.log(error);
     }
 })();
